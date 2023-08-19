6c86f47feeb49321c7ca950c7c288a9eb117e406
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-08-19 14:25:39 +0200

    update

--- challenger_gui/src/views/UsersView.vue
+++ challenger_gui/src/views/UsersView.vue
@@ -6,7 +6,7 @@
         <div class="card" style="width: 18rem;">
             <img src="" class="card-img-top" alt="">
             <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text"></p>
             </div>
           </div>
     </div>
@@ -22,6 +22,7 @@
         },
         mounted() {
             this.$store.dispatch('fetchUsers')
+            console.log(this.users);
         }
     }
 </script>
